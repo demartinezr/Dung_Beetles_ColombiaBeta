f49284777a135b44b93476d11860a2d1e0797e15
Diego Martinez <diemartinezre@unal.edu.co>
2024-08-25 16:06:00 -0500

    upload R code

--- 1_GBIF_search/GBIF_records_search.R
+++ 1_GBIF_search/GBIF_records_search.R
@@ -10,10 +10,10 @@
     library(sf)
 
 # Dung beetles Dataset #
-    IBDdata <- read_excel("C:/Users/Dell-PC/Dropbox/CO_DBdata/Scarabaeinae_database_2024.xlsx", sheet="Scarabaeinae_database")
+    IBDdata <- read_excel("C:/Users/Dell-PC/Dropbox/CO_DBdata/abundance/Scarabaeinae_database_2024.xlsx", sheet="Scarabaeinae_database_2024")
 #
 # List of species and subspecies #
-    species <- data.frame(subset(IBDdata, taxonRank == "species"))
+    species <- data.frame(subset(IBDdata, taxonRank == "Species"))
     species <- sort(unique(species$scientificName)) 
     species <- gsub("_", " ", species)
     subspecies <- data.frame(subset(IBDdata, taxonRank == "Subspecies"))
@@ -24,14 +24,14 @@
 # GBIF search #
     result <- occ_data(scientificName = species, hasCoordinate = TRUE, synonym = TRUE)
     data_list <- lapply(result, function(x) x$data)
-    records <- bind_rows(data_list, .id = "scientificName1")
+    records <- bind_rows(data_list, .id = "scientificName")
     #names(records)
     #write.table(records, "GBIF_2024-02-13.txt", row.names = FALSE, sep=";")
 #
 # Check records #
 #
 # subset records of Deltochilum carinatum
-    d_car <- data.frame(subset(records, scientificName1 == "Deltochilum carinatum"))
+    d_car <- data.frame(subset(records, scientificName == "Deltochilum carinatum"))
     View(d_car)
 #
 # base maps #
