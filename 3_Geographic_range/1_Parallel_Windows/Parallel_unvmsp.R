698e12fb78e020b101dcbd349ca9a3ed9cbc71bb
Diego Martinez <diemartinezre@unal.edu.co>
2024-08-25 20:49:44 -0500

    upload R code

--- 3_Geographic_range/1_Parallel_Windows/Parallel_unvmsp.R
+++ 3_Geographic_range/1_Parallel_Windows/Parallel_unvmsp.R
@@ -1,98 +1,98 @@
-# Loop for obtaining geographic ranges from GBIF records, ecoregions, and 
-# altitudinal ranges (DEM)
-setwd("D:/Doctorado/Tesis/GBIF")
+# Function for obtaining geographic ranges based on fieldwork and literature, 
+# aecoregions, nd altitudinal ranges (alos palsar DEM 30m) for unique species 
+# of the project.
+#
+  setwd("C:/Users/Dell-PC/Dropbox/CO_DBdata")
 #
 # R packages
-library(raster)
-library(dplyr)
-library(ggplot2)
-library(rgdal)
-library(sp)
-library(rgeos)
-library(terra)
-library(foreach)
-library(doParallel)
-
+    library(raster)
+    library(dplyr)
+    library(ggplot2)
+    library(rgdal)
+    library(sp)
+    library(rgeos)
+    library(terra)
+    library(foreach)
+    library(doParallel)
+#
 # Base maps
-ecoreg <- readOGR(dsn = "D:/Capas/World/wwf/Ecoregions2017", layer = "eco_col") # País
-ecoreg <- spTransform(ecoreg, CRS("+proj=longlat +datum=WGS84")) # WGS84
-dem_ALOS <- "D:/Capas/coldem30/alos_dem_col.tif"
-dem <- raster(dem_ALOS)
-
+  ecoreg <- readOGR(dsn = "D:/Capas/World/wwf/Ecoregions2017", layer = "eco_col") # País
+  ecoreg <- spTransform(ecoreg, CRS("+proj=longlat +datum=WGS84")) # WGS84
+  dem_ALOS <- "D:/Capas/coldem30/alos_dem_col.tif"
+  dem <- raster(dem_ALOS)
+#
 # DB data
-datos <- read.csv("Scarabaeinae_database.csv", header =TRUE, sep=";")
-proj_crs <- CRS("+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs")  
-registros <- data.frame(subset(datos, taxonRank == "Unverified morphospecies")) 
-registros <- registros %>%
-  rename(
-    decimalLatitude = lat_all_points,
-    decimalLongitude = lon_all_points
-  )
-registros$decimalLatitude <- as.numeric(registros$decimalLatitude)
-registros$decimalLongitude <- as.numeric(registros$decimalLongitude)
-registros$elev_ALOS_all_points <- as.numeric(registros$elev_ALOS_all_points)
-
+  datos <- read.csv("Scarabaeinae_database.csv", header =TRUE, sep=";")
+  proj_crs <- CRS("+proj=utm +zone=18 +datum=WGS84 +units=m +no_defs")  
+  registros <- data.frame(subset(datos, taxonRank == "Unverified morphospecies")) 
+  registros <- registros %>%
+    rename(
+      decimalLatitude = lat_all_points,
+      decimalLongitude = lon_all_points
+    )
+  registros$decimalLatitude <- as.numeric(registros$decimalLatitude)
+  registros$decimalLongitude <- as.numeric(registros$decimalLongitude)
+  registros$elev_ALOS_all_points <- as.numeric(registros$elev_ALOS_all_points)
+#
 # Species subset
-G1 <- unique(registros$scientificName)[[36]] 
-
+  G1 <- unique(registros$scientificName)[c(1:10)] 
+#
 # Function to process each species
-process_species <- function(especie) {
-  cada_especie <- registros[registros$scientificName == especie, ]
-  
+  process_species <- function(especie) 
+{
+    cada_especie <- registros[registros$scientificName == especie, ]
+  #
   # GBIF records by species
-  coordenadas <- cada_especie[, c("decimalLongitude", "decimalLatitude")]
-  coordenadas <- na.omit(coordenadas)
-  coordenadas <- coordenadas[!duplicated(coordenadas), ]
-  puntos <- SpatialPoints(coordenadas, proj4string = CRS("+proj=longlat +datum=WGS84"))
-  coordenadas <- as.data.frame(puntos)
-  
+    coordenadas <- cada_especie[, c("decimalLongitude", "decimalLatitude")]
+    coordenadas <- na.omit(coordenadas)
+    coordenadas <- coordenadas[!duplicated(coordenadas), ]
+    puntos <- SpatialPoints(coordenadas, proj4string = CRS("+proj=longlat +datum=WGS84"))
+    coordenadas <- as.data.frame(puntos)
+  #
   # Polygon based on the number of records 1, 2, 3 ...
-  if (nrow(coordenadas) <= 2) {
-    if (nrow(coordenadas) == 1) {
-      punto <- SpatialPoints(coordenadas, proj4string = CRS("+proj=longlat +datum=WGS84"))
-      punto <- spTransform(punto, CRS("+proj=longlat +datum=WGS84 +no_defs"))
-      poligono_sp <- gBuffer(punto, width = 0.1)
+    if (nrow(coordenadas) <= 2) {
+      if (nrow(coordenadas) == 1) {
+        punto <- SpatialPoints(coordenadas, proj4string = CRS("+proj=longlat +datum=WGS84"))
+        punto <- spTransform(punto, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+        poligono_sp <- gBuffer(punto, width = 0.1)
+      } else {
+        linea <- SpatialLines(list(Lines(list(Line(coordenadas)), "1")), proj4string = CRS("+proj=longlat +datum=WGS84"))
+        linea <- spTransform(linea, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+        poligono_sp <- gBuffer(linea, width = 0.1)
+        proj4string(poligono_sp) <- CRS("+proj=longlat +datum=WGS84")
+      }
     } else {
-      linea <- SpatialLines(list(Lines(list(Line(coordenadas)), "1")), proj4string = CRS("+proj=longlat +datum=WGS84"))
-      linea <- spTransform(linea, CRS("+proj=longlat +datum=WGS84 +no_defs"))
-      poligono_sp <- gBuffer(linea, width = 0.1)
+      indices_poligono <- chull(coordenadas$decimalLongitude, coordenadas$decimalLatitude)
+      poligono <- coordenadas[indices_poligono, ]
+      poligono_sp <- SpatialPolygons(list(Polygons(list(Polygon(poligono)), "1")))
       proj4string(poligono_sp) <- CRS("+proj=longlat +datum=WGS84")
     }
-  } else {
-    indices_poligono <- chull(coordenadas$decimalLongitude, coordenadas$decimalLatitude)
-    poligono <- coordenadas[indices_poligono, ]
-    poligono_sp <- SpatialPolygons(list(Polygons(list(Polygon(poligono)), "1")))
-    proj4string(poligono_sp) <- CRS("+proj=longlat +datum=WGS84")
-  }
-  
-  pol_sol <- tryCatch(gIntersects(ecoreg, poligono_sp, byid = TRUE), error = function(e) NULL)
-  if (!is.null(pol_sol)) {
-    indice_sol <- which(pol_sol)
-    if (length(indice_sol) > 0) {
-      sol_com <- ecoreg[indice_sol, ]
-      sol_com <- spTransform(sol_com, proj_crs)
-      sol_com <- gBuffer(sol_com, byid = TRUE, width = 0)
-      sol_com <- spTransform(sol_com, CRS("+proj=longlat +datum=WGS84 +no_defs"))
-      contorno <- gUnaryUnion(sol_com)
-      
-      # Adjust contour to the altitudinal limits of the species
-      dem_contorno <- mask(dem, contorno)
-      altura_max <- max(cada_especie$elev_ALOS_all_points, na.rm = TRUE) + 100
-      altura_min <- max(min(cada_especie$elev_ALOS_all_points, na.rm = TRUE) - 100, 0)
-      raster_altitud_ajustada <- dem_contorno >= altura_min & dem_contorno <= altura_max
-      spat_raster <- terra::rast(raster_altitud_ajustada)
-      
-      # Raster to polygon
+    pol_sol <- tryCatch(gIntersects(ecoreg, poligono_sp, byid = TRUE), error = function(e) NULL)
+    if (!is.null(pol_sol)) {
+      indice_sol <- which(pol_sol)
+      if (length(indice_sol) > 0) {
+        sol_com <- ecoreg[indice_sol, ]
+        sol_com <- spTransform(sol_com, proj_crs)
+        sol_com <- gBuffer(sol_com, byid = TRUE, width = 0)
+        sol_com <- spTransform(sol_com, CRS("+proj=longlat +datum=WGS84 +no_defs"))
+        contorno <- gUnaryUnion(sol_com)
+  #
+  # Adjust contour to the altitudinal limits of the species
+    dem_contorno <- mask(dem, contorno)
+    altura_max <- max(cada_especie$elev_ALOS_all_points, na.rm = TRUE) + 100
+    altura_min <- max(min(cada_especie$elev_ALOS_all_points, na.rm = TRUE) - 100, 0)
+    raster_altitud_ajustada <- dem_contorno >= altura_min & dem_contorno <= altura_max
+    spat_raster <- terra::rast(raster_altitud_ajustada)
+    # Raster to polygon
       dem_ajustado <- terra::as.polygons(spat_raster)
       poligonos_valor_1 <- dem_ajustado[dem_ajustado$layer == 1, ]
       poligono_union <- terra::aggregate(poligonos_valor_1)
-      
-      # Save
-      nombre_archivo <- paste0("poligono_", gsub(" ", "_", especie), ".shp")
-      ruta_archivo <- file.path("D:/Doctorado/Tesis/GBIF/contorno", nombre_archivo)
-      writeVector(poligono_union, ruta_archivo, filetype = "ESRI Shapefile")
-      
-      return(nombre_archivo)
+  #
+  # Save
+    nombre_archivo <- paste0(gsub(" ", "._", especie), ".shp")
+    ruta_archivo <- file.path("D:/Doctorado/Tesis/GBIF/contorno", nombre_archivo)
+    writeVector(poligono_union, ruta_archivo, filetype = "ESRI Shapefile")
+    return(nombre_archivo)
     } else {
       cat("No overlapping polygons found for:", especie, "\n")
       return(NULL)
@@ -102,24 +102,24 @@ process_species <- function(especie) {
     return(NULL)
   }
 }
-
+#
 # Set up parallel backend to use 4 cores
-cl <- makeCluster(4)
-registerDoParallel(cl)
-
-# Parallel processing
-resultados_especies <- foreach(especie = G1, .combine = 'c', .packages = c('raster', 'rgdal', 'sp', 'rgeos', 'terra', 'dplyr')) %dopar% {
-  process_species(especie)
-}
-
-# Stop the cluster
-stopCluster(cl)
-
-# Convert the list of results to a named list
-names(resultados_especies) <- G1
-
-# Filter out NULL results
-resultados_especies <- resultados_especies[!sapply(resultados_especies, is.null)]
+  cl <- makeCluster(4)
+  registerDoParallel(cl)
+  # Parallel processing
+    resultados_especies <- foreach(especie = G1, .combine = 'c', .packages = c('raster', 'rgdal', 'sp', 'rgeos', 'terra', 'dplyr')) %dopar% 
+    {
+    process_species(especie)
+    }
+  #
+  # Stop the cluster
+    stopCluster(cl)
+  #
+  # Convert the list of results to a named list
+    names(species_result) <- G1
+  #
+  # Filter out NULL results
+    species_result <- species_result[!sapply(species_result, is.null)]
 
 #Check records
 par(mar=c(2,2,1,1)) 
---
+++ 4_Traits/Morphometric_measurements.R
@@ -0,0 +1,66 @@
+# Morphometric measurements
+setwd("D:/Doctorado/Tesis/traits")
+#
+# packages
+library(readxl)
+library(dplyr)
+#
+# dataset new photos morphometrics
+  data_new <- read_excel("C:/Users/Dell-PC/Dropbox/CO_DBdata/template_sheet_morph_traits_2024.xlsx", sheet="Sheet1")
+  data_new$backtibialength <- as.numeric(data_new$backtibialength)
+  data_new$backspurlength <- as.numeric(data_new$backspurlength)
+# filter and mean by individual_id_number para elytrawidth, bodywidth, elytralength, bodylength
+  mean_body_elytra <- data_new  %>%
+  filter(grepl("_1|_2$", photo_number)) %>%
+  group_by(scientificName, individual_id_number) %>%
+  summarize(across(c(elytrawidth, bodywidth, elytralength, bodylength), mean, na.rm = TRUE), .groups = "drop")
+# filter and mean by individual_id_number para frontfemurlength, fronttibialength, backfemurlength, backtibialength, backspurlength
+  mean_legs <- data_new %>%
+  filter(grepl("_3|_4$", photo_number)) %>%
+  group_by(scientificName, individual_id_number) %>%
+  summarize(across(c(frontfemurlength, fronttibialength, backfemurlength, backtibialength, backspurlength), mean, na.rm = TRUE), .groups = "drop")
+# merge by individual_id_number y scientificName
+  means_new <- merge(mean_body_elytra, mean_legs, 
+                     by = c("scientificName", "individual_id_number"))
+#
+# dataset 2021 morphometrics
+  data_2021 <- read_excel("C:/Users/Dell-PC/Dropbox/CO_DBdata/output_allmorphmetrics_withsppnames_2024.xlsx", sheet = "output_allmorphmetrics_withsppn")
+#
+# Intersect colnames between data frames
+  common_cols <- intersect(names(data_2021), names(means_new))
+# select common columns between data frames
+  data_2021_common <- data_2021 %>%
+  select(all_of(common_cols))
+  means_new_common <- means_new %>%
+  select(all_of(common_cols))
+# Join dataframes based on common columns
+  morphometrics <- bind_rows(data_2021_common, means_new_common)
+  morphometrics <- morphometrics %>%
+  arrange(scientificName)
+# traits body size and leg ratio measurements
+  morphometrics$bodysize <- as.numeric(morphometrics$elytrawidth*
+                                         morphometrics$bodylength)
+  morphometrics$legratio <- as.numeric((morphometrics$frontfemurlength+
+       morphometrics$fronttibialength)/
+         (morphometrics$backfemurlength+
+          morphometrics$backtibialength+
+          morphometrics$backspurlength))
+  morphometrics_mean <- morphometrics %>%
+    group_by(scientificName) %>%
+    summarise(
+      across(
+        .cols = c(bodysize, legratio),
+        .fns = ~ mean(.x[!is.na(.x) & !is.nan(.x) & !is.infinite(.x) & .x != 0], na.rm = TRUE),
+        .names = "{col}"
+      )
+    ) %>%
+    ungroup()
+#
+# Merge morphometrics measurements and behaviour traits in Dung beetles data set
+ DB_data <- read_excel("C:/Users/Dell-PC/Dropbox/CO_DBdata/Scarabaeinae_database_2024.xlsx", sheet="Scarabaeinae_database_2024")
+ behaviour <- read_excel("C:/Users/Dell-PC/Dropbox/CO_DBdata/DB_Distributions_traits_2024.xlsx", sheet="DB_Distributions_traits")
+ behaviour <- behaviour[c("scientificName", "nest_guild", "diet_range", "activity")]
+ DB_data <- left_join(DB_data, morphometrics_mean, by = "scientificName") 
+ DB_data <- left_join(DB_data, behaviour, by = "scientificName")
+#write.table(DB_data, "Scarabaeinae_database_2024.txt")
+ 
